a7fc62636ba605524b206b1e6c18d4d1a36d6911
Eloston <Eloston@users.noreply.github.com>
2015-10-03 21:06:54 -0700

    Fixed patches for current commit Added scripts for testing and making patches

---
+++ debian/scripts/create_patch.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+TEXT_EDITOR_CMD=nano
+TEXT_EDITOR_DIFF_CMD="nano -"
+DIFF_CMD="git diff --no-index"
+FILE_PATH=$1
+
+if [[ -z "${FILE_PATH}" ]]; then
+    echo "Usage: $0 <FILE_TO_PATCH>"
+    exit
+fi
+
+if [[ ! -f ${FILE_PATH} ]]; then
+    echo "File does not exist."
+    exit
+fi
+
+cp ${FILE_PATH} ${FILE_PATH}.orig
+${TEXT_EDITOR_CMD} ${FILE_PATH}
+read -p "Press [Enter] to continue:"
+${DIFF_CMD} ${FILE_PATH}.orig ${FILE_PATH} | tail --lines=+3 | sed "s|${FILE_PATH}.orig|${FILE_PATH}|g" | ${TEXT_EDITOR_DIFF_CMD}
+mv ${FILE_PATH}.orig ${FILE_PATH}
